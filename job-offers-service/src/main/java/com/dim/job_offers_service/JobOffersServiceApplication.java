03dab74fcbaa5cc27087672c32f51e667f4f28f0
matheossiandimitri <matheossian.dimitri@gmail.com>
2024-09-12 21:11:03 +0200

    init with 3 services

---
+++ job-offers-service/src/main/java/com/dim/job_offers_service/JobOffersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dim.job_offers_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOffersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobOffersServiceApplication.class, args);
+    }
+
+}
