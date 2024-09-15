2724994dccc499b4e8ff7463e3b799711af38cef
matheossiandimitri <matheossian.dimitri@gmail.com>
2024-09-15 16:16:25 +0200

    configure gateway with eureka

---
+++ gateway-service/src/main/java/com/dim/gateway_service/config/WebClientConfig.java
@@ -0,0 +1,16 @@
+package com.dim.gateway_service.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+class WebClientConfig {
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
+    }
+}
--- gateway-service/src/main/java/com/dim/gateway_service/routers/JobOffersServiceRouter.java
+++ gateway-service/src/main/java/com/dim/gateway_service/routers/JobOffersServiceRouter.java
@@ -1,15 +1,18 @@
-package com.dim.gateway_service;
+package com.dim.gateway_service.routers;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
-public class Routing {
+public class JobOffersServiceRouter {
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("job-offers-service", r -> r.path("/job-offers-service/**").uri("lb://JOB-OFFERS-SERVICE"))
+                .route(r -> r.path("/job-offers-service")
+                        // remove service name
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://job-offers-service"))
                 .build();
     }
 }
